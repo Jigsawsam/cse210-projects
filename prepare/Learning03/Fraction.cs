7070aab82589eeb5c009eb2b1609e49bcc6644a5
Jigsawsam <bgt117@gmail.com>
2024-01-23 14:41:23 -0800

    Learning03 complete

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,36 @@
+using System;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int numerator)
+    {
+        _top = numerator;
+        _bottom = 1;
+    }
+
+    public Fraction(int numerator, int denominator)
+    {
+        _top = numerator;
+        _bottom = denominator;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+
+}
