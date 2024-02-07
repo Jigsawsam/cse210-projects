cb941000baa8c7444b343c7a5601f28379eeb8a0
Jigsawsam <bgt117@gmail.com>
2024-02-07 00:53:53 -0800

    Learning05 complete

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Console.Clear();
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Blue", 3);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Red", 2, 5);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Purple", 2);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape area is {area}.");
+        }
     }
 }
